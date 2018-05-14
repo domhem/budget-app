6a9043d04419a0da3337b37a49db59c2f0131b01
Daniel Bravo <bravoappdev@gmail.com>
2018-05-14 14:55:39 -0700

    Fix update dom on new expense/income

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,33 +1,70 @@
 import Controller from '@ember/controller';
+import $ from 'jquery';
+
+function rerender(obj, parent, user, inc) {
+  var mainNode = obj.$('<div>');
+  var subNode = obj.$('<div>');
+  var descNode = obj.$('<div>');
+  var amtNode = obj.$('<div>');
+
+  mainNode.append(descNode);
+  mainNode.append(subNode);
+  subNode.append(amtNode);
+
+  mainNode.addClass('item clearfix');
+  subNode.addClass('right clearfix');
+  descNode.addClass('item__description');
+  amtNode.addClass('item__value');
+
+  if(inc) {
+    descNode.text(user.income_description);
+    amtNode.text(user.income);
+  } else {
+    descNode.text(user.expense_description);
+    amtNode.text(user.expense);
+  }
+
+  obj.$(parent).append(mainNode);
+
+}
+
 export default Controller.extend({
+  init() {
+    this._super(...arguments);
+    this.$ = $;
+  },
   actions: {
     saveIncome() {
       // getting email from session
       var email = this.get('session.currentUser.email');
-      var user = this.store.createRecord('index',{
+      var userData = {
         income_description:this.get('income_description'),
         income:this.get('income'),
         email:email
-      });
+      };
+      var user = this.store.createRecord('index', userData);
       user.save();
 
       this.set('income', '');
       this.set('income_description', '');
+      rerender(this, '#income_list:last-child', userData, 1);
       //this.set('display_income', this.get('income'));
     },
 
     saveExpense() {
       // getting email from session
       var email = this.get('session.currentUser.email');
-      var user = this.store.createRecord('index',{
+      var userData = {
         expense_description:this.get('expense_description'),
         expense:this.get('expense'),
         email:email
-      });
+      };
+      var user = this.store.createRecord('index', userData);
       user.save();
 
       this.set('expense', '');
       this.set('expense_description', '');
+      rerender(this, '#expense_list:last-child', userData);
       //this.set('display_income', this.get('income'));
     },
 
